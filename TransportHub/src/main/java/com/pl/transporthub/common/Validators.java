d5b40297280d953fb3d7ea633628be700355a9b0
sh1ft <sh1ft@pavels-mbp>
2019-04-22 20:11:44 +0300

    Added Copyright

--- TransportHub/src/main/java/com/pl/transporthub/common/Validators.java
+++ TransportHub/src/main/java/com/pl/transporthub/common/Validators.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (C) 2017 Pavel Mayzenberg and Amos Refael
+ * Copyright (C) 2019 Pavel Mayzenberg, Leon Peper, Oded Levin
  * 
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
