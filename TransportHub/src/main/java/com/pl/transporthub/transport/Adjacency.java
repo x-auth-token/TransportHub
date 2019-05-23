5a228999b69ea51e825586d59a529a755adc3c45
LeonPeper <user@DESKTOP-S5USTJ2>
2019-05-23 14:33:53 +0300

    adjancy matrix

--- TransportHub/src/main/java/com/pl/transporthub/transport/Adjacency.java
+++ TransportHub/src/main/java/com/pl/transporthub/transport/Adjacency.java
@@ -18,8 +18,8 @@ public class Adjacency {
 		return adj;
 	}
 
-	public void setAdj(Integer[][] adj) {
-		this.adj = adj;
+	private void setAdj(Integer x, Integer y) {
+		this.adj[x][y] = 1;
 	}
 	
 	
@@ -27,13 +27,29 @@ public class Adjacency {
 	
 	void init_adjValues() {
 		
-		for(int i=0; i<this.getAdj().length; i++) {
-			
-			for(int j=0; j<this.getAdj().length; j++) {
-				
-				
-			}
-		}
+		this.createAdj(17);
+		
+		this.setAdj(0, 4);
+		this.setAdj(1, 3);
+		this.setAdj(1, 4);
+		this.setAdj(2, 3);
+		this.setAdj(3, 5);
+		this.setAdj(3, 10);
+		this.setAdj(4, 6);
+		this.setAdj(4, 5);
+		this.setAdj(5, 8);
+		this.setAdj(5, 7);
+		this.setAdj(6, 7);
+		this.setAdj(7, 12);
+		this.setAdj(8, 9);
+		this.setAdj(9, 15);
+		this.setAdj(10, 11);
+		this.setAdj(11, 9);
+		this.setAdj(11, 16);
+		this.setAdj(12, 13);
+		this.setAdj(12, 14);
+		
+		
 		
 	}
 	
