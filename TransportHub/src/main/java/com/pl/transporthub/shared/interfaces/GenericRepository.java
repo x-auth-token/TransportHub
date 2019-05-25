d386276c452287378a3cb2dcb249e2123c000f3f
sh1ft <sh1ft@pavels-mbp>
2019-05-25 17:39:54 +0300

    .

--- TransportHub/src/main/java/com/pl/transporthub/shared/interfaces/GenericRepository.java
+++ TransportHub/src/main/java/com/pl/transporthub/shared/interfaces/GenericRepository.java
@@ -1,5 +1,9 @@
 package com.pl.transporthub.shared.interfaces;
 
+import org.eclipse.persistence.jpa.jpql.parser.TrimExpression.Specification;
+
+import com.pl.transporthub.user.User;
+
 // Generic Repository Class using Java Generics
 public interface GenericRepository<T> {
 	
@@ -7,7 +11,7 @@ public interface GenericRepository<T> {
 	void remove(T t);
 	T get(T t);
 	Iterable <T> getAll();
-	Iterable <T> findAll(T t);
+	Iterable<T> find(Specification spec);
 	
 
 }
--- TransportHub/src/main/java/com/pl/transporthub/user/UserRepository.java
+++ TransportHub/src/main/java/com/pl/transporthub/user/UserRepository.java
@@ -1,37 +1,55 @@
 package com.pl.transporthub.user;
 
+import javax.persistence.EntityManager;
+import javax.persistence.criteria.Expression;
+
+import org.eclipse.persistence.jpa.jpql.parser.TrimExpression.Specification;
+
 import com.pl.transporthub.shared.interfaces.GenericRepository;
 
 public class UserRepository implements GenericRepository<User>{
+	
+	private EntityManager em;
+	
+	public UserRepository(EntityManager em) {
+		
+		this.em = em;
+	}
 
 	@Override
-	public void add(User t) {
+	public void add(User u) {
+		
+		if (u.getUsername() == null) {
+			em.persist(u);
+		} else {
+			u = em.merge(u);
+		}
+		
+	}
+
+	@Override
+	public void remove(User u) {
 		// TODO Auto-generated method stub
 		
 	}
 
 	@Override
-	public void remove(User t) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public User get(User t) {
+	public User get(User u) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
 	public Iterable<User> getAll() {
-		// TODO Auto-generated method stub
-		return null;
+		
+		return em.createQuery("SELECT * from Users u", User.class).getResultList();
 	}
 
 	@Override
-	public Iterable<User> findAll(User t) {
-		// TODO Auto-generated method stub
+	public Iterable<User> find(Specification spec) {
+		
 		return null;
 	}
+
 
 }
