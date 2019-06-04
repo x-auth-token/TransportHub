e03328eb625408c6409c79716ada868301c5cd59
PavelMayzenberg <pmayzenberg@gmail.com>
2019-06-04 19:58:32 +0300

    Added Repository Classes

---
+++ TransportHub/src/main/java/com/pl/transporthub/transport/StationRepository.java
@@ -0,0 +1,39 @@
+package com.pl.transporthub.transport;
+
+import org.eclipse.persistence.jpa.jpql.parser.TrimExpression.Specification;
+
+import com.pl.transporthub.shared.interfaces.GenericRepository;
+
+public class StationRepository implements GenericRepository<Station>{
+
+	@Override
+	public void add(Station t) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void remove(Station t) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Station get(Station t) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Iterable<Station> getAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Iterable<Station> find(Specification spec) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
