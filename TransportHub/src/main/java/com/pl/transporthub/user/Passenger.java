b22f52abf6ba4b2e5aca8298a534a3d63246676d
sh1ft <sh1ft@pavels-mbp>
2019-07-15 12:58:22 +0300

    a

--- TransportHub/src/main/java/com/pl/transporthub/user/Passenger.java
+++ TransportHub/src/main/java/com/pl/transporthub/user/Passenger.java
@@ -15,10 +15,35 @@ public class Passenger extends AuthenticatedUser {
 	 * public Passenger(String un, char[] pass, boolean adminValue) { super(un,
 	 * pass, adminValue); // TODO Auto-generated constructor stub }
 	 */
-	
+	private String mobileNumber;
+	private String passportID;
 	public Passenger(String un, char[] pass, Date expirationDate, boolean adminValue) {
 		super(un, pass, expirationDate, adminValue);
 		// TODO Auto-generated constructor stub
+	}
+	/**
+	 * @return the mobileNumber
+	 */
+	public String getMobileNumber() {
+		return mobileNumber;
+	}
+	/**
+	 * @param mobileNumber the mobileNumber to set
+	 */
+	public void setMobileNumber(String mobileNumber) {
+		this.mobileNumber = mobileNumber;
+	}
+	/**
+	 * @return the passportID
+	 */
+	public String getPassportID() {
+		return passportID;
+	}
+	/**
+	 * @param passportID the passportID to set
+	 */
+	public void setPassportID(String passportID) {
+		this.passportID = passportID;
 	}
 
 	
